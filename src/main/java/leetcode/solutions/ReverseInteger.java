4a7c32376463569087236263ba63fa6ec7f8c5ca
BrahianVT <pumasemj@hotmail.com>
2020-07-28 22:14:19 -0600

    added ReverseInteger

---
+++ src/main/java/leetcode/solutions/ReverseInteger.java
@@ -0,0 +1,31 @@
+package leetcode.solutions;
+
+
+/**
+ Number leecode problem: 7
+   https://leetcode.com/problems/reverse-integer/
+    Time Complexity: O(n)
+   Space Complexity: O(1)
+*/
+public class ReverseInteger{
+	
+	public int reverse(int x) {
+		
+		int res = 0, aux = 0;
+		
+		while(x != 0){
+			
+			int digit = x %10;
+			
+			aux = res* 10 + digit;
+			
+			// Validate the overflow
+			if(res != (aux - digit) /10){ return 0; }
+			
+			res = aux; x/10;
+		}
+		
+		return res;
+	}
+	
+}
