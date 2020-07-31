77edd0b8454475b53840701d71ab0ba63911c292
BrahianVT <pumasemj@hotmail.com>
2020-07-30 21:38:21 -0600

    added PalindromeNumber

--- src/main/java/leetcode/solutions/PalindromeNumber.java
+++ src/main/java/leetcode/solutions/PalindromeNumber.java
@@ -7,5 +7,19 @@ package leetcode.solutions;
 */
 
 public PalindromeNumber{
-	
+
+	public boolean isPalindrome(int x) {
+		if( x < 0 || x%10 == 0) return false;
+		
+		if(x == 0) return true;
+		
+		int invert = 0;
+		
+		while(x > invert){
+			invert = inver * 10 + x%10;
+			x/=10;
+		}
+		
+		return (x == invert || x == inver/10);
+	}
 }
