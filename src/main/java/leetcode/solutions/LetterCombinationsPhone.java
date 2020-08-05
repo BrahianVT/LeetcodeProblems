c334c80687aee839f54f591d9039761d525cc5fe
BrahianVT <pumasemj@hotmail.com>
2020-08-05 00:22:10 -0600

    backtraking approach

---
+++ src/main/java/leetcode/solutions/LetterCombinationsPhone.java
@@ -0,0 +1,21 @@
+
+package leetcode.Solutions;
+
+/**
+   Number leecode problem: 17
+   Link: https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+   
+   Time Complexity: O(n^4)
+   Space Complexity: O(n^4)
+*/
+
+
+public class LetterCombinationsPhone{
+	
+	
+	public List<String> letterCombinations(String digits) {
+		
+		return;
+		
+	}
+}
---
+++ src/main/java/leetcode/solutions/Sum4.java
@@ -0,0 +1,16 @@
+
+package leetcode.Solutions;
+
+/**
+   Number leecode problem: 18
+   Link: https://leetcode.com/problems/4sum/
+   
+   Time Complexity: O(n^4)
+   Space Complexity: O(n^4)
+*/
+
+public class Sum4{
+	
+	 public List<List<Integer>> fourSum(int[] nums, int target) { }
+	
+}
