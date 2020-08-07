8799662f69f4ba86729837b76348bfd31b44d2c2
BrahianVT <pumasemj@hotmail.com>
2020-08-06 22:15:48 -0600

    added LetterCombinationsPhone

--- src/main/java/leetcode/solutions/LetterCombinationsPhone.java
+++ src/main/java/leetcode/solutions/LetterCombinationsPhone.java
@@ -15,7 +15,26 @@ public class LetterCombinationsPhone{
 	
 	public List<String> letterCombinations(String digits) {
 		
-		return;
+		List<String> res = new ArrayList<>();
+		char[][] map = {{},{},{'a','b','c'},{'d','e','f'},{'g','h','i'},{'j','k','l'},{'m','n','o'},{'p','q','r','s'},{'t','u','v'},{'w','x','y','z'}};
 		
+		backtracking(digits, map, start, new StringBuilder(), res);
+		
+		return res;
+		
+	}
+	
+	public void backtracking(String digits, char[][] map, int start, StringBuilder str, List<String> res){
+
+		if(start == digits.length()){ res.add(str.toString());  return; }
+
+		int i = digits.charAt(start) - '0'; 
+		
+		for(int j = 0; j < map[i].length; j++){
+			str.append(digits.charAt(start));
+			
+			backtracking)(digits, map, j + 1, str, res);
+			str.deleteCharAt(str.length() - 1);
+		}
 	}
 }
