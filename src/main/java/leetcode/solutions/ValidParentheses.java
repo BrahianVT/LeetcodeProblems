2377e5536724cd46fb6dad7ecaa7c8d8d2a5ccaa
BrahianVT <pumasemj@hotmail.com>
2020-08-09 19:37:35 -0600

    added ValidParentheses

---
+++ src/main/java/leetcode/solutions/ValidParentheses.java
@@ -0,0 +1,41 @@
+
+
+
+package leetcode.Solutions;
+
+import java.util.*;
+
+/**
+   Number leecode problem:20
+   Link: https://leetcode.com/problems/valid-parentheses/
+   
+   Time Complexity: O(n)
+   Space Complexity: O(n)
+*/
+
+public class ValidParentheses{
+	
+	 public boolean isValid(String s) {
+		 
+		Stack<Character> stack = new Stack<Character>();
+		
+		for(char c : s.toCharArray()){
+			if(c == '}'){
+				if(stack.isEmpty()){ return false; }
+				else if('{' != stack.pop()){ return false; }
+			}
+			else if(c == ')'){
+				if(stack.isEmpty()){ return false; }
+				else if('(' != stack.pop()){ return false; }				
+			}
+			else if(c == ']'){
+				if(stack.isEmpty()){ return false; }
+				else if('[' != stack.pop()){ return false; }
+			}
+		}
+
+		
+		return stack.isEmpty();
+	 }
+	
+}
