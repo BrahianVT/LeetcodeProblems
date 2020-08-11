96ec6d27cb77359a0d05d4623f2fb07a9b3ecac9
BrahianVT <pumasemj@hotmail.com>
2020-08-10 19:00:10 -0600

    added MergeTwoSortedLists

---
+++ src/main/java/leetcode/solutions/MergeTwoSortedLists.java
@@ -0,0 +1,33 @@
+
+
+
+package leetcode.Solutions;
+
+import java.util.*;
+
+/**
+   Number leecode problem:20
+   Link: https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+   
+   Time Complexity: O(n)
+   Space Complexity: O(n)
+*/
+
+public class MergeTwoSortedLists{
+	
+	
+	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+		
+		if(l1 == null){ return l2; }
+		if(l2 == null){ return l1; }
+		
+		if(l1.val < l2.val){
+			l1.next = mergeTwoLists(l1.next, l2);
+			return l1;
+		} else {
+			l2.next = mergeTwoLists(l2.next, l2); 
+			return l2;
+		}
+	}
+      
+}
