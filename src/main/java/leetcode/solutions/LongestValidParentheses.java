45de5ead78c5df35304354c6630cb77c7e34e6ec
BrahianVT <pumasemj@hotmail.com>
2020-08-20 23:44:38 -0600

    Added LongestValidParentheses

---
+++ src/main/java/leetcode/solutions/LongestValidParentheses.java
@@ -0,0 +1,33 @@
+package leetcode.solutions;
+import java.util.*;
+/**
+ Number leecode problem: 32
+	https://leetcode.com/problems/longest-valid-parentheses/
+    Time Complexity: O(n)
+    Space Complexity: O(n)
+*/
+
+
+public class LongestValidParentheses{
+	
+	int longestValidParentheses(string s) {
+		
+		Stack<Integer> stack = new Stack();
+		
+		int longest = 0;
+		for(int i = 0; i < s.length; i++){
+			char c = s.charAt(i);
+			if( c == '('){
+				stack.push(i);
+			} else if( stack.isEmpty() || s.charAt(stack.peek()) == ')'){
+				stack.push(i);
+			} else {
+				stack.pop();
+				longest = Math.max(i - (stack.isEmpty()?-1:stack.peak()), max);
+			}
+		}
+		
+		
+		return longest;
+	}
+}
