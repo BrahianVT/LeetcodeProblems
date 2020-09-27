52612dd7a141c0b147eb9ea0145ea0a62b01182e
BrahianVT <pumasemj@hotmail.com>
2020-09-27 16:34:38 -0600

    added evaluate-division

---
+++ src/main/java/leetcode/solutions/EvaluateDivision.java
@@ -0,0 +1,18 @@
+
+
+package leetcode.solutions;
+import java.util.*;
+/**
+ Number leecode problem: 399
+	https://leetcode.com/problems/evaluate-division/
+    Time Complexity: O(E + V)
+    Space Complexity: O(E + V)
+	because is always a constant 9
+*/
+
+public class EvaluateDivision{
+	
+	 public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+		 
+	 }
+}
