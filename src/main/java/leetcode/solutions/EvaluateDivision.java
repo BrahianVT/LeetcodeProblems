90f5c0ce002164ce56381fc19201c4884dc94996
BrahianVT <pumasemj@hotmail.com>
2020-09-28 20:44:45 -0600

    added EvaluateDivision

--- src/main/java/leetcode/solutions/EvaluateDivision.java
+++ src/main/java/leetcode/solutions/EvaluateDivision.java
@@ -5,14 +5,43 @@ import java.util.*;
 /**
  Number leecode problem: 399
 	https://leetcode.com/problems/evaluate-division/
-    Time Complexity: O(E + V)
-    Space Complexity: O(E + V)
-	because is always a constant 9
+    Time Complexity: O(Q(E + V)) Q:Size of queries
+    Space Complexity: O(Q(E + V))
 */
 
 public class EvaluateDivision{
 	
-	 public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-		 
-	 }
+	public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+		Map<String, Map<String, Double>> graph = new HashMap<>();
+		double[] res = new double[queries.size()];
+
+		for(int i = 0; i < equations.size(); i++){
+			String from = equations.get(i).get(0), to = equations.get(i).get(1);
+			graph.computeIfAbsent(from, v -> new HashMap()).put(to, values[i]);
+			graph.computeIfAbsent(to, v -> new HashMap()).put(from, 1/values[i]);
+		}
+		
+		for(int i = 0; i < res.length; i++){
+			String from = queries.get(i)(0), to = queries.get(i)(1);
+			res[i] = dfs(from, to ,1, graph, new HashSet()); 
+			
+		}
+		
+		return res;
+	}
+	
+	private double dfs(String from, String to, double aux, Map<String, Map<String, Double>> graph, Set<String> visited){
+		if(!graph.containsKey(from) || !visited.add(from)){ return -1; }
+		
+		if(from.equals(to)) return aux;
+		
+		for(String node: graph.get(from).keySet()){
+			double res = dfs(node, to, aux * graph.get(from).get(node), graph, visited);
+			if(res != -1) return res;
+		}
+		
+		return -1;
+	}
 }
+
+
