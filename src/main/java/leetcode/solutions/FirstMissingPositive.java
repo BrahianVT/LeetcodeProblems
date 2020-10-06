1d983f9281f6b04c1a24bc277d2cc0c4ccc5b95e
BrahianVT <pumasemj@hotmail.com>
2020-10-05 20:21:48 -0600

    added FirstMissingPositive

---
+++ src/main/java/leetcode/solutions/FirstMissingPositive.java
@@ -0,0 +1,38 @@
+
+
+package leetcode.solutions;
+import java.util.*;
+/**
+ Number leecode problem: 41
+
+	https://leetcode.com/problems/first-missing-positive/
+     Time Complexity: O(n)
+    Space Complexity: O(1)
+*/
+
+public class FirstMissingPositive{
+	
+	public class int firstMissingPositive(int[] nums){
+		int n = nums.length;
+		
+		for(int i = 0; i < n; i++){
+			if(nums[i] > n || nums[i] <= 0){ nums[i] = n + 1; }
+		}
+		
+		for(int i = 0; i < n; i++){
+			int index = Math.abs(nums[i]);
+			if(index > n){ continue; }
+			else{
+				index--;
+				if(nums[index] > 0){ nums[index] = -nums[index]; }
+			}
+		}
+		
+		
+		for(int i = 0; i < n; i++){
+			if(nums[i] > 0){ return i + 1; }
+		}
+		
+		return n + 1;
+	}
+}
