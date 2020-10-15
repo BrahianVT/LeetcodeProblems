d0283e5f5f6b72394f1627e4cf9e8550b33af983
BrahianVT <pumasemj@hotmail.com>
2020-10-14 22:22:40 -0600

    added RotateList

---
+++ src/main/java/leetcode/solutions/RotateList.java
@@ -0,0 +1,32 @@
+
+/**
+ Number leecode problem: 61
+
+	https://leetcode.com/problems/rotate-list/
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+*/
+
+public class RotateList{
+	
+	public ListNode rotateRight(ListNode head, int k){
+		if(heag == null) return head;
+		
+		List aux = new LitsNode(0);
+		aux.next = head;
+		ListNode last = aux , elementAtK = aux;
+		
+		int size = 0;
+		while(last.next != null){ size++; last = last.next; }
+		
+		for(int i = size - k%size; j > 0; j--){
+			elementAtK = elementAtK.next;
+		}
+		
+		aux.next = elementAtK.next;
+		last.next = aux.next;
+		elementAtK.next = null;
+		
+		return aux.next;
+	}
+}
