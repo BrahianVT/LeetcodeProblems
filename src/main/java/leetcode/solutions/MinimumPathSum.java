b91c036119751eb16418ead36f2a0826211f7e23
BrahianVT <pumasemj@hotmail.com>
2020-10-14 22:47:36 -0600

    added MinimumPathSum

---
+++ src/main/java/leetcode/solutions/MinimumPathSum.java
@@ -0,0 +1,33 @@
+
+
+/**
+ Number leecode problem: 64
+
+	https://leetcode.com/problems/minimum-path-sum/
+    Time Complexity: O(nm)
+    Space Complexity: O(nm)
+*/
+public class MinimumPathSum {
+	
+	public int minPathSum(int[][] grid){
+		if(grid.length == 0) return 0;
+		
+		int n = grid.length, m = grid[0].length; 
+		int dp[][] = new int[n][m];
+		
+		int aux = 1000000;
+		for(int i = 0; i < n + 1; i++){ dp[i][0] = aux; }
+		for(int j = 0; j < m + 1; j++){ dp[0][j] = aux; }
+		
+		dp[1][0] = 0;
+		
+		 for(int i = 1; i <= n; i++){
+			for(int j = 1; j <= m; j++){
+				int min = Math.min(dp[i - 1][j] + grid[i - 1][j - 1], dp[i][j - 1] + dp[i - 1][j - 1]);
+				dp[i][j]+=min;
+			}
+		 }
+		 
+		return dp[n][m];
+	}
+}
---
+++ src/main/java/leetcode/solutions/UniquePathsII.java
@@ -0,0 +1,26 @@
+
+/**
+ Number leecode problem: 63
+
+	https://leetcode.com/problems/unique-paths-ii/
+    Time Complexity: O(nm)
+    Space Complexity: O(nm)
+*/
+
+public class UniquePathsII{
+
+	public int uniquePathsWithObstacles(int[][] obstacleGrid){
+		int n = obstacleGrid.length, m = obstacleGrid[0].length;
+		int[][] dp = new int[n + 1][ m + 1];
+		
+		dp[1][0] = 1;
+		for(int i = 1; i <= n; i++){
+			for(int j = 1; j <= m; j++){
+				if(obstacleGrid[i - 1][j - 1] == 1) continue;
+				dp[i][j] = dp[i -1 ][ j] + dp[i][j - 1];
+			}
+		}
+		
+		return dp[n][m];
+	}
+}
