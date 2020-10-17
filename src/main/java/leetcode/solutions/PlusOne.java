777bc1e0fd0ffe026dd80aa079ab5a0e277c021e
BrahianVT <pumasemj@hotmail.com>
2020-10-17 17:39:10 -0600

    added PlusOne

---
+++ src/main/java/leetcode/solutions/PlusOne.java
@@ -0,0 +1,29 @@
+
+
+/**
+ Number leecode problem: 66
+	https://leetcode.com/problems/plus-one/
+    Time Complexity: O(n)
+    Space Complexity: O(n)
+*/
+
+public class PlusOne{
+	
+	public int[] plusOne(int[] digits){
+		int[] res;
+		boolean oneMore = true;
+		for(int n: digits){ if(n != 9){ oneMore = false; break; } }
+		res = (oneMore)?new int[digits.length + 1]:new int[digits.length];
+		
+		int sum = 1;
+		int indexR = res.length - 1;
+		for(int i = digits.length - 1; i >= 0; i--){
+			sum[i]+= digits[i];
+			res[indexR--] = sum%10;
+			sum/=10;
+		}
+		
+		if(sum > 0){ res[0] = 1; }
+		return res;
+	}
+}
