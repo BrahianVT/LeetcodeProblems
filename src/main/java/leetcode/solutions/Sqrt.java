d344b3b2c4127c54ea2d80b5d8ab887f7ae80392
BrahianVT <pumasemj@hotmail.com>
2020-10-17 18:05:47 -0600

    added Sqrt

---
+++ src/main/java/leetcode/solutions/Sqrt.java
@@ -0,0 +1,15 @@
+
+/**
+ Number leecode problem: 69
+	https://leetcode.com/problems/add-binary/
+    Time Complexity: O(1)
+    Space Complexity: O(1)
+*/
+
+public class Sqrt{
+	
+	public int mySqrt(int x){
+		double res = Math.pow((double)x, 0.5);
+		return (int) f;
+	}
+}
