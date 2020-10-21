d08d7076d08020bd1dbfe6551add982f3882a81b
BrahianVT <pumasemj@hotmail.com>
2020-10-20 20:47:56 -0600

    added ClimbingStairs

---
+++ src/main/java/leetcode/solutions/ClimbingStairs.java
@@ -0,0 +1,21 @@
+/**
+ Number leecode problem: 70
+	https://leetcode.com/problems/add-binary/
+    Time Complexity: O(1)
+    Space Complexity: O(1)
+*/
+public class ClimbingStairs{
+	public int climbStairs(int n){
+		if(n == 0 || n == 1){ return 1; }
+		
+		int stepOne = 1, stepTwo = 1, res = 0;
+		
+		for(int i = 2; i <= n; i++){
+			res = stepOne + stepTwo;
+			stepOne = stepTwo;
+			stepTwo = res;
+		}
+		
+		return res;
+	}
+}
