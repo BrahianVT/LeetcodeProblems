574bc653f059dc60deadded9b416d66388652d06
BrahianVT <pumasemj@hotmail.com>
2020-10-22 20:57:17 -0600

    Added EditDistance

---
+++ src/main/java/leetcode/solutions/EditDistance.java
@@ -0,0 +1,29 @@
+package leetcode.solutions;
+import java.util.*;
+/**
+ Number leecode problem: 72
+ 
+	https://leetcode.com/problems/edit-distance/
+    Time Complexity: O(n*m)
+    Space Complexity: O(n*m)
+*/
+
+
+
+public class EditDistance{
+	
+	public int minDistance(String word1, String word2){
+		int[] dp = new int[word1.length() + 1][word2.length() + 1];
+		
+		for(int i = 0; i < word1.length(); i++){ dp[i][0] = i; }
+		for(int i = 0; i < word2.length(); i++){ dp[0][i] = i; }
+		
+		for(int i = 0; i < word1.length(); i++){
+			for(int j = 0; j < word2(); j++){
+				dp[i + 1][ j + 1] = Math.min(dp[i][j], Math.min(dp[i +1][j], dp[i][ j + 1])) + 1;
+			}
+		}
+		
+		return dp[word1.length][word2.length];
+	}
+}
