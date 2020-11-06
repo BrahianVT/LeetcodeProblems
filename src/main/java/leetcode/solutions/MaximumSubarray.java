63a5d7fc808e181b57333adb310e5b36a8bfd16c
BrahianVT <pumasemj@hotmail.com>
2020-11-05 20:19:28 -0600

    fixed MaximumSubarray

--- src/main/java/leetcode/solutions/MaximumSubarray.java
+++ src/main/java/leetcode/solutions/MaximumSubarray.java
@@ -8,10 +8,13 @@ import java.util.*;
     Space Complexity: O(1)
 */
 public class MaximumSubarray{
-	int maxSoFar = nums[0], maxEndingHere = nums[0];
-	
-	for(int i = 1; i < nums.length; i++){
-		maxEndingHere = Math.max(maxEndingHere + nums[i], nums[i]);
-		maxSoFar = Math.max(maxSoFar, maxEndingHere);
+	 public int maxSubArray(int[] nums) {
+		int maxSoFar = nums[0], maxEndingHere = nums[0];
+		for(int i = 1; i < nums.length; i++){
+			maxEndingHere = Math.max(maxEndingHere + nums[i], nums[i]);
+			maxSoFar = Math.max(maxSoFar, maxEndingHere);
+		}
+		
+		return maxSoFar;
 	}
 }
