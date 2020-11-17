c75603087dfbeca4422cb79c68b342ea46115d98
BrahianVT <pumasemj@hotmail.com>
2020-11-16 18:08:46 -0600

    added  Scramble String

---
+++ src/main/java/leetcode/solutions/ScrambleString.java
@@ -0,0 +1,31 @@
+
+package leetcode.solutions;
+import java.util.*;
+/**
+ Number leecode problem: 87
+	https://leetcode.com/problems/scramble-string/
+    Time Complexity: O(2^n)
+    Space Complexity: O(2^n)
+	
+*/
+
+ public class ScrambleString{
+	public boolean isScramble(String s1, String s2){
+		if(s1.equals(s2))return true;
+		int[] s1Arr = int[26], s2Arr = new int[26];
+		
+		int s = s1.length();
+		for(int i = 0; i < s; i++){ s1Arr[s1.charAt(i)]++; s2Arr[s2.charAt(i)]++; }
+		for(int i = 0; i < s; i++ ){ if(s1Arr[i] != s2Arr[i]) return false; }
+		
+		for(int i = 1; i < s; i++){
+			if(isScramble(s1.substring(0,i), s2.substring(0,i)) &&
+			   isScramble(s1.substring(i), s2.substring(i)))
+			   return true;
+			
+			if(isScramble(s1.substring(0,i), s2.substring(s - i)) &&
+			   isScramble(s1.substring(i), s2.substring(0, s - 1)))
+			   return true;
+		}
+	}
+ }
