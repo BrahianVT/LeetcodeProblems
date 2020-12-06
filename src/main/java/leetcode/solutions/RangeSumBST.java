bc661c7402d1a6582b8239c5de6b290782a0d5e1
BrahianVT <pumasemj@hotmail.com>
2020-12-05 18:19:26 -0600

    added /RangeSumBST

---
+++ src/main/java/leetcode/solutions/RangeSumBST.java
@@ -0,0 +1,22 @@
+
+
+package leetcode.solutions;
+import java.util.*;
+/**
+	Number leecode problem: 938
+	https://leetcode.com/problems/range-sum-of-bst/
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+*/
+
+ public class RangeSumBST{
+	
+	public int rangeSumBST(TreeNode root, int low, int high){
+		if(root == null) return 0;
+		
+		if(root.val > high) rangeSumBST(root.left, low,right);
+		if(root.val < low) rangeSumBST(root.right, low, right);
+		
+		return rangeSumBST(root.left,low, high) + rangeSumBST(root.right, low, high) + root.val;
+	}
+ }
