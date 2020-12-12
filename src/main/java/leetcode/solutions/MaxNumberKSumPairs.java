928f82904052e328518ca1e3e30821f9345ccdaa
BrahianVT <pumasemj@hotmail.com>
2020-12-12 16:35:20 -0600

    added MaxNumberKSumPairs

---
+++ src/main/java/leetcode/solutions/MaxNumberKSumPairs.java
@@ -0,0 +1,25 @@
+
+package leetcode.solutions;
+import java.util.*;
+/*
+	Number leecode problem: 1679
+	https://leetcode.com/problems/max-number-of-k-sum-pairs/
+    Time Complexity: O(nlogn)
+    Space Complexity: O(1)
+*/
+
+public class MaxNumberKSumPairs{
+	public int maxOperations(int[] nums, int k) {
+		Arrays.sort(nums);
+		int i = 0, j = nums.length -1, res = 0;
+		for(i < j){
+			if(nums[i] + nums[j] > k)j--;
+			else if(nums[i] + nums[j] < k)i++;
+			else{
+				i++;j++; res++;
+			}
+		}
+		
+		return res;
+	}
+}
