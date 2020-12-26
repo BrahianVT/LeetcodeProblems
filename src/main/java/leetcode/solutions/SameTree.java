d52462eeefddf3b9e9eb262d2a9dda20a3fd088e
BrahianVT <pumasemj@hotmail.com>
2020-12-26 14:24:40 -0600

    fixed a bug

--- src/main/java/leetcode/solutions/SameTree.java
+++ src/main/java/leetcode/solutions/SameTree.java
@@ -10,9 +10,8 @@ https://leetcode.com/problems/same-tree/
 public class SameTree{
 	public boolean isSameTree(TreeNode p, TreeNode q){
 		if(p == null && q == null)return true;
-		else return false;
-		 
-		 if(p.val != q.val)return false;
+		else if(p == null || q == null || p.val != q.val)return false;
+		
 		return isSameTree(p.left, q.left) && isSameTree(p.right, q.right); 
 	}
 }
