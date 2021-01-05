9bdca82bb86e13c3369be81c42f5a2e21ead27bf
Brahian <pumasemj@hotmail.com>
2021-01-04 18:24:21 -0600

    Update BestTimeBuySellStockIV.java

--- src/main/java/leetcode/solutions/BestTimeBuySellStockIV.java
+++ src/main/java/leetcode/solutions/BestTimeBuySellStockIV.java
@@ -12,7 +12,7 @@ public class BestTimeBuySellStockIV{
 		if(k >= prices.length){
 			int profit = 0;
 			for(int i = 1; i < prices.length; i++){ 
-				if(prices[i] > prices[i-1])profit+=prices[i] > prices[i-1];
+				if(prices[i] > prices[i-1])profit+=prices[i] - prices[i-1];
 			}
 			return profit;
 		}
