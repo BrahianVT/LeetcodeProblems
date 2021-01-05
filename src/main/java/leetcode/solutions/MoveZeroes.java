19a38a4320fa0a42c17ca20607c088bbc1cf53ba
BrahianVT <pumasemj@hotmail.com>
2021-01-04 21:16:04 -0600

    added BinaryTreeMaximumPathSum

---
+++ src/main/java/leetcode/solutions/MoveZeroes.java
@@ -0,0 +1,24 @@
+package leetcode.solutions;
+import java.util.*;
+/*
+	Number leecode problem:283
+	https://leetcode.com/problems/move-zeroes/
+    Time Complexity: O(n)
+    Space Complexity: O(n)
+*/
+
+
+public class MoveZeroes{
+	public void moveZeroes(int[] nums) {
+		
+		int len = 0, cont = 0;
+		for(int i = 0; i < nums.length; i++){
+			if(nums[i] == 0){ cont++; continue; }
+			nums[len++] = nums[i];
+		}
+		int last = nums.length -1;
+		while(cont-- > 0){
+			nums[last--] = 0;
+		}
+	}
+}
