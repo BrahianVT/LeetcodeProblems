0a979b06882485f92c3989b5eb0d10d75bb47d94
BrahianVT <pumasemj@hotmail.com>
2021-01-19 20:00:38 -0600

    Added  BaseballGame

---
+++ src/main/java/leetcode/solutions/BaseballGame.java
@@ -0,0 +1,29 @@
+
+package leetcode.solutions;
+import java.util.*;
+/*
+	Number leecode problem: 602
+	https://leetcode.com/problems/baseball-game/
+    Time Complexity: O(n)
+    Space Complexity: O(n)
+*/
+public class BaseballGame{
+	public int calPoints(String[] ops){
+		Stack<Integer> s = new Stack<>();
+		int res = 0;
+		for(String o: ops){
+			case "+":
+				Integer prev = s.pop(), next = prev + s.peek();
+			break;
+			case "D":
+				s.push(s.peek() * 2);
+			break;
+			default:
+			s.push(Integer.parseInt(o));
+			break;
+		}
+		
+		while(!s.isEmpty())res+=s.pop();
+		return res;
+	}
+}
