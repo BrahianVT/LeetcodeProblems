5167c0c677f0fefeb5e17b3f9730c956777c5c4b
BrahianVT <pumasemj@hotmail.com>
2021-01-20 20:54:05 -0600

    Added MaximumUnitsTruck

---
+++ src/main/java/leetcode/solutions/MaximumUnitsTruck.java
@@ -0,0 +1,26 @@
+package leetcode.solutions;
+import java.util.*;
+/*
+	Number leecode problem: 1710
+	https://leetcode.com/problems/maximum-units-on-a-truck/
+    Time Complexity: O(n log n)
+    Space Complexity: O(n log n)
+*/
+
+public class MaximumUnitsTruck{
+	public int maximumUnits(int[][] boxTypes, int truckSize){
+		Arrays.sort(boxTypes, (p1,p2) -> p1[1] == p2[1]?p1[0]-p2[0]:p2[1]- p1[1]);
+		int res = 0, index = 0;
+		while(index < boxTypes.length && truckSize > 0){
+			int size = boxTypes[index][0];
+			if(size <= truckSize){
+				res+= size * boxTypes[index][1];
+			} else {
+				res+= truckSize * boxTypes[index][1];
+			}
+			index++;
+		}
+		
+		return res;
+	}
+}
