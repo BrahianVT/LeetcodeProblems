924b1665777f1ae110f4699bffd2fce4c04f6ee6
BrahianVT <pumasemj@hotmail.com>
2021-01-25 20:40:29 -0600

    added SlowestKey

---
+++ src/main/java/leetcode/solutions/SlowestKey.java
@@ -0,0 +1,26 @@
+package leetcode.solutions;
+import java.util.*;
+/*
+	Number leecode problem: 1629
+	https://leetcode.com/problems/sudoku-solver/
+    Time Complexity: O(n)
+    Space Complexity: O(n)
+*/
+public class SlowestKey{
+	public char slowestKey(int[] releaseTimes, String keysPressed) {
+		int aux = 0, res = 0;
+		char out = '_';
+		char[] keys = keysPressed.toCharArray();
+		for(int i = 0; i < keys.length; i++){
+			int time = releaseTimes[i]- aux;
+			aux = releaseTimes;
+			if(time > aux){
+				res = time; out = keys[i];
+			}
+			else if(time == aux && keys[i] > out)out = keys[i];
+		}
+		
+		return out;
+	}
+	
+}
