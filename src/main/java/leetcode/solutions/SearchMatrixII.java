9f4a8638285677321aa025eb2918d920a866cdc6
Brahian <pumasemj@hotmail.com>
2021-02-15 17:37:46 -0600

    Update SearchMatrixII.java

--- src/main/java/leetcode/solutions/SearchMatrixII.java
+++ src/main/java/leetcode/solutions/SearchMatrixII.java
@@ -10,7 +10,8 @@ import java.util.*;
 
 public class SearchMatrixII{
 	public boolean searchMatrix(int[][] matrix,int target){
-		int s = 0, end = matrix[0] -1;
+		
+		int s = 0, end = matrix[0].length -1;
 		while(s < matrix.length && end >= 0){
 			int m = matrix[s][end];
 			if(m == target)return true;
