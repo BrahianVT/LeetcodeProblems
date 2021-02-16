4fe8ab12b67246e9d69f478ccb0ec67fca3f20c3
BrahianVT <pumasemj@hotmail.com>
2021-02-15 20:28:58 -0600

    fixed a bug

--- src/main/java/leetcode/solutions/SumSubarrayMinimums.java
+++ src/main/java/leetcode/solutions/SumSubarrayMinimums.java
@@ -10,7 +10,7 @@ import java.util.*;
 public class SumSubarrayMinimums{
 	public int sumSubArray(int[] arr){
 		Stack<Integer> s = new Stack<Integer>();
-		s.stack(-1);
+		s.push(-1);
 		int[] dp = new int[arr.length + 1];
 		int mod = 1000000007, res = 0; 
 		for(int i = 0; i < arr.length; i++){
@@ -19,7 +19,7 @@ public class SumSubarrayMinimums{
 			dp[i+ 1] = (dp[ s.peek() + 1] + (i - s.peek()) * arr[i])% mod;
 			s.push(i);
 			res+= dp[i + 1];
-			res%=cost;
+			res%=mod;
 		}
 		return res;
 	}
