77df76a7935ea85456a2d152f192516a59a3cbc4
Brahian <pumasemj@hotmail.com>
2021-02-17 14:29:49 -0600

    Update Sum3.java

--- src/main/java/leetcode/solutions/Sum3.java
+++ src/main/java/leetcode/solutions/Sum3.java
@@ -17,26 +17,18 @@ public class Sum3{
 	public List<List<Integer>> threeSum(int[] nums) {
 		
 		Arrays.sort(nums);
-		
 		List<List<Integers>> res = new ArrayList();
-		
 		for(int i = 0; i <= nums.length -3; i++){
-			
 			if(i == 0 || nums[i] == nums[i-1]){
 				int start = i + 1, end  = nums.length -1, sum = 0 - nums[i];
-				
 				while( start < end){
-					
 					int aux = nums[start] + nums[end];
 					if(aux == sum){
-						
 						res.add(Arrays.asList(nums[i]), nums[start], nums[end]);
 						while( start < end && nums[start] == nums[start + 1])
-							start++;
-						
+							start++;	
 						while(start < end && nums[end] == nums[end -1])
-							end--;
-						
+							end--;				
 						start++; end--;
 					}
 					else if(aux < sum) {
@@ -46,8 +38,6 @@ public class Sum3{
 						end--;
 					}
 				}
-				
-				
 			}
 		}
 		return res;
