fa2f366faa31d91edae2fcbc2517b8a519bed116
Brahian <pumasemj@hotmail.com>
2021-03-22 12:18:10 -0600

    Update BinaryTreePaths.java

--- src/main/java/leetcode/solutions/BinaryTreePaths.java
+++ src/main/java/leetcode/solutions/BinaryTreePaths.java
@@ -11,23 +11,23 @@ import java.util.*;
 public class BinaryTreePaths{
 	public List<String> binaryTreePaths(TreeNode root) {    
         List<String> res = new ArrayList<>();
-
+	dfsFindPaths(root, res, "");
         return res;
 	}
 	
 	private void dfsFindPaths(TreeNode root, List<String> res, String aux){
 		
-		if(root == null)res.add(aux + root.val); return;
+		if(root == null)res.add(aux); return;
 		
 		if(root.left != null){
 			StringBuilder l = new StringBuilder(aux);
 			l.append("->").append(root.val);
-			dfsFindPaths(root, res, l.toString());
+			dfsFindPaths(root.left, res, l.toString());
 		}
 		if(root.right != null){
 			StringBuilder r = new StringBuilder(aux);
 			r.append("->").append(root.val);
-			dfsFindPaths(root, res, r.toString());
+			dfsFindPaths(root.right, res, r.toString());
 		}
 	}
 
