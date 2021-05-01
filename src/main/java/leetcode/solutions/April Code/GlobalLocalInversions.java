6f6b60071a42975b9dc18a17a769c00de5d2089f
BrahianVT <pumasemj@hotmail.com>
2021-04-30 23:18:26 -0600

    added leetcode challenge 5

---
+++ src/main/java/leetcode/solutions/April Code/GlobalLocalInversions.java
@@ -0,0 +1,19 @@
+
+
+package leetcode.solutions;
+import java.util.*;
+/*
+	4
+	Number leecode problem:775
+	https://leetcode.com/problems/global-and-local-inversions/
+    Time Complexity: O(n) 
+    Space Complexity: O(1)
+*/
+public class GlobalLocalInversions{
+	public boolean isIdealPermutation(int[] A) {
+		for(int i = 0; i < A.length; i++){
+			if(Math.abs(i - A[i]) > 1)return false;
+		}
+		return true;		
+    }
+}
