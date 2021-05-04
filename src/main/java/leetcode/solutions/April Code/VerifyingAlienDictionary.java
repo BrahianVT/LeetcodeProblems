64fc2d677d0bca9b15f750052a5a50228966db4e
BrahianVT <pumasemj@hotmail.com>
2021-05-03 22:49:20 -0600

    added leetcode challenge 9

---
+++ src/main/java/leetcode/solutions/April Code/VerifyingAlienDictionary.java
@@ -0,0 +1,39 @@
+
+
+package leetcode.solutions;
+import java.util.*;
+/*
+	4
+	Number leecode problem:953
+	https://leetcode.com/problems/verifying-an-alien-dictionary/
+    Time Complexity: O(n) 
+    Space Complexity: O(n)
+*/
+public class VerifyingAlienDictionary{
+	
+	public boolean isAlienSorted(String[] words, String order) {
+		int[] map = new char[26];
+		int i = 0;
+		for(char c: order.toCharArray(order)){
+			map[c - 'a'] = i++;
+		}
+		
+		for(int i = 0; i < words.length -1; i++){
+			if(!isSorted(words[i].toCharArray(), words[i+1].toCharArray(),map))return false;
+		}
+			
+		return true;
+    }
+	
+	private boolean isSorted(char[] c1, char[] c2, int[]  map){
+		int n = Math.min(c1.length, c2.length);
+		int i = 0;
+		while(i < n){
+			if(c1[i] != c2[i]){
+				return map[c1[i - 'a']] <  map[c2[i - 'a']];
+			}
+			i++;
+		}
+		return c1.length = n;
+	}
+}
