92aba453cb511cb2310675b7c8fb5bb81d4edbb9
BrahianVT <pumasemj@hotmail.com>
2021-05-05 21:26:21 -0600

    added leetcode challenge 14

---
+++ src/main/java/leetcode/solutions/April Code/PartitionList.java
@@ -0,0 +1,29 @@
+
+package leetcode.solutions;
+import java.util.*;
+/*
+	14
+	Number leecode problem: 86
+	https://leetcode.com/problems/partition-list/
+    Time Complexity: O(n) 
+    Space Complexity: O(n)
+*/
+
+public class PartitionList{
+	
+	public ListNode partition(ListNode head, int x) {
+		ListNode res = new ListNode(0);
+		ListNode l = new ListNode(0), r = new ListNode(0);
+		ListNode l2 = l, r2 = r;
+		while(head != null){
+			if(head.val < x){
+				l2.next = head; l2 = l2.next;
+			} else {
+				r2.next = head; r2 = r2.next;
+			}
+		}
+		s1.next = r2.next;
+		s2.next = null;
+		return l.next;
+    }
+}
