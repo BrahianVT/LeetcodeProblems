ecf301b8dab6f61f69200f3338ab5a61a9303ec9
BrahianVT <pumasemj@hotmail.com>
2021-05-07 20:44:29 -0600

    added leetcode challenge 19

---
+++ src/main/java/leetcode/solutions/April Code/CombinationSumIV.java
@@ -0,0 +1,34 @@
+
+
+
+package leetcode.solutions;
+import java.util.*;
+/*
+	19
+	Number leecode problem: 377
+https://leetcode.com/problems/combination-sum-iv/
+    Time Complexity: O(2^n) 
+    Space Complexity: O(target)
+*/                                 
+public class CombinationSumIV {
+	public int combinationSum4(int[] nums, int target){
+		Arrays.sort(nums);
+		int[] dp = new int[target + 1];
+		Arrays.fill(dp, -1);
+		dp[0] = 1;
+		return calculate(nums, target, dp);
+	}
+	
+	private int calculate(int[] nums, int target, int[] dp){
+		if(dp[target] != -1)return dp[target];
+		int res = 0;
+		for(int i = 0; i < nums.length; i++){
+			if(nums[i] <= target)
+				res+= calculate(nums, nums[i]- target, dp);
+			else 
+				break;
+		}
+		
+		return dp[target] = res;
+	}
+}
