7597678efde30df39fea415080bed11437950d79
BrahianVT <pumasemj@hotmail.com>
2021-05-10 23:24:04 -0600

    added leetcode challenge 25

---
+++ src/main/java/leetcode/solutions/April Code/FurthestBuildingYouCanReach.java
@@ -0,0 +1,26 @@
+
+package leetcode.solutions;
+import java.util.*;
+/*
+	26
+	Number leecode problem: 1642
+https://leetcode.com/problems/furthest-building-you-can-reach/
+    Time Complexity: O(nlogn) 
+    Space Complexity: O(n)
+*/
+
+public class FurthestBuildingYouCanReach{
+	public int furthestBuilding(int[] heights, int bricks, int ladders) {
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		for(int i = 0; i < heights.length-1; i++){
+			int diff = heights[i+1] - heights[i];
+			if(diff > 0)
+				pq.offer(diff);
+			if(pq.size() > ladders)
+				bricks-= pq.poll();
+			if(bricks < 0)
+				return i;
+		}
+		return heights.length -1;
+	}
+}
