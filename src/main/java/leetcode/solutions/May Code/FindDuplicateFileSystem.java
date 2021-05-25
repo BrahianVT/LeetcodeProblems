3074dd5618703b14b0631fb72cfbf96e0d567367
BrahianVT <pumasemj@hotmail.com>
2021-05-24 22:00:23 -0600

    code challenge 18

---
+++ src/main/java/leetcode/solutions/May Code/FindDuplicateFileSystem.java
@@ -0,0 +1,33 @@
+package leetcode.solutions;
+import java.util.*;
+/*
+	18
+	Number leecode problem: 609
+	https://leetcode.com/problems/find-duplicate-file-in-system/
+	Time Complexity: O(n)
+	Space Complexity: O(n)
+*/   
+
+public class FindDuplicateFileSystem{
+	
+ public List<List<String>> findDuplicate(String[] paths) {
+        
+        Map<String, List<String>> map = new HashMap<>();
+        for(String p : paths){
+            String[] w = p.split(" ");
+            for(int i = 1; i < w.length; i++){
+                int j = w.indexOf("(");
+                String aux = w.substring(j+1, w[i].length - 1);
+                
+                map.computeIfAbsent(w[i], v -> new ArrayList()).add(w[0] + "/" + w[i].substring(j));
+            }
+        }
+        
+        List<List<String>> res = new ArrayList();
+        
+        for(Map.Entry<String, List<String>> e : map.entrySet())
+            res.add(e.getValue());
+        
+        return res;
+    }
+}
