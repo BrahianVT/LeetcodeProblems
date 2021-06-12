f0e224569639e01aa0f7535791c8b0b196199b63
BrahianVT <pumasemj@hotmail.com>
2021-06-12 16:33:54 -0600

    Added problem 6

---
+++ src/main/java/leetcode/solutions/June Code/MinCostClimbingStairs.java
@@ -0,0 +1,24 @@
+
+package leetcode.solutions;
+import java.util.*;
+/*
+	Problem : 7
+	Number leecode problem: 128
+	https://leetcode.com/problems/min-cost-climbing-stairs/
+    Time Complexity: O(n) 
+    Space Complexity: O(0)
+*/
+
+public class MinCostClimbingStairs{
+	public int minCostClimbingStairs(int[] cost) {
+     
+		int one = cost[0], two = cost[1];
+		for(int i = 2; i < cost.length; i++){
+			int curr = cost[i] + Math.min(one,two);
+			one = two;
+			two = curr;
+		}
+		
+		return Math.min(one,two);
+    }
+}
