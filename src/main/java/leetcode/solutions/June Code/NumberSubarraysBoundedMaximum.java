8298ef9b8c2adbbb80796b925f393e1e70417968
BrahianVT <pumasemj@hotmail.com>
2021-06-21 21:13:39 -0600

    added problem 17

---
+++ src/main/java/leetcode/solutions/June Code/NumberSubarraysBoundedMaximum.java
@@ -0,0 +1,27 @@
+
+
+
+package leetcode.solutions;
+import java.util.*;
+/*
+	Problem : 17
+	Number leecode problem: 795
+	https://leetcode.com/problems/number-of-subarrays-with-bounded-maximum/
+    Time Complexity: O(n) 
+    Space Complexity: O(1)
+*/
+
+public class NumberSubarraysBoundedMaximum{
+	
+	public int numSubarrayBoundedMax(int[] nums, int left, int right) {
+        int res = 0, sum = 0,a = 0;
+		for(int i = 0; i < nums.length; i++){
+			if(n > right){ sum = 0;  a = i + 1;}
+			else if(n >= left){
+				sum = i - a + 1;
+			}
+			res += sum;
+		}
+		return res;
+    }
+}
