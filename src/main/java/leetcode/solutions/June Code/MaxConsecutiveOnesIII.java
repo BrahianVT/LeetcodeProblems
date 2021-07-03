903ae5022397df13828637d41d1004fa92a3c32b
BrahianVT <pumasemj@hotmail.com>
2021-07-02 20:56:56 -0600

    added problem 29

---
+++ src/main/java/leetcode/solutions/June Code/MaxConsecutiveOnesIII.java
@@ -0,0 +1,16 @@
+package leetcode.solutions;
+import java.util.*;
+/*
+	Problem : 29
+	Number leecode problem: 1004
+	https://leetcode.com/problems/max-consecutive-ones-iii/
+    Time Complexity: O(1) 
+    Space Complexity: O(1)
+*/
+
+public class MaxConsecutiveOnesIII{
+
+	public int longestOnes(int[] nums,int k){
+		
+	}
+}
