a6eefcc4c3b2aedd97bae57761495781d905e5d3
BrahianVT <pumasemj@hotmail.com>
2021-07-05 20:40:53 -0600

    .

--- src/main/java/leetcode/solutions/June Code/MaxConsecutiveOnesIII.java
+++ src/main/java/leetcode/solutions/June Code/MaxConsecutiveOnesIII.java
@@ -4,13 +4,21 @@ import java.util.*;
 	Problem : 29
 	Number leecode problem: 1004
 	https://leetcode.com/problems/max-consecutive-ones-iii/
-    Time Complexity: O(1) 
+    Time Complexity: O(n) 
     Space Complexity: O(1)
 */
 
 public class MaxConsecutiveOnesIII{
 
 	public int longestOnes(int[] nums,int k){
+		int i = 0, j = 0;
 		
+		for(; j  < nums.length; i++){
+			if(nums[j] == 0)
+				k--;
+			if(k < 0 && nums[i++] == 0)k++;
+		}
+		
+		return j-i;
 	}
 }
