dd4493e684175cff0cf19354fb082bf9356ae156
BrahianVT <pumasemj@hotmail.com>
2021-07-10 18:00:49 -0600

    added problem 6

--- src/main/java/leetcode/solutions/July Code/ReduceArraySizeHalf.java
+++ src/main/java/leetcode/solutions/July Code/ReduceArraySizeHalf.java
@@ -5,12 +5,37 @@ import java.util.*;
 	Problem : 6
 	Number leecode problem: 1338
 	https://leetcode.com/problems/count-vowels-permutation/
-    Time Complexity: O(nm) 
-    Space Complexity: O(nm)
+    Time Complexity: O(n log n) 
+    Space Complexity: O(n)
 */
 
 
 public class ReduceArraySizeHalf{
-
+	
+	 public int minSetSize(int[] arr) {
+		Arrays.sort(arr);
+		int l = 0 ,r = 0 , n = arr.length;
+		Integer[] len = new Integer[n];
+		int iLen = 0;
+		
+		for(; r < n; r++){
+			if(arr[r] != arr[l]){
+				len[iLen++] = r - l;
+				l = r;
+			}
+		}
+		len[iLen++] = r - l;
+		
+		for(; iLen < n; iLen++)len[iLen] = 0;
+		
+		Arrays.sort(len, (p1, p2) -> p2 - p1);
+		int cont = n;;
+		iLen = 0;
+		while(cont > n/2){
+			cont-= len[iLen];
+		}
+		
+		return iLen;
+     }
 	
 }
