44280d55ae03cfcdea5a5e7107c52c376f310c02
BrahianVT <pumasemj@hotmail.com>
2021-07-17 18:46:58 -0600

    added problem 13

---
+++ src/main/java/leetcode/solutions/July Code/FindPeakElement.java
@@ -0,0 +1,26 @@
+package leetcode.solutions;
+import java.util.*;
+/*
+	Problem : 13
+	Number leecode problem: 162
+	https://leetcode.com/problems/isomorphic-strings/
+    Time Complexity: O(n log n) 
+    Space Complexity: O()
+*/
+	public class FindPeakElement{
+		public int findPeakElement(int[] nums) {
+			
+			int l = 0, r = nums.length-1;
+			while(l  < r){
+				int m = (l + r) / 2;
+				
+				if(nums[m] < nums[m+1])
+					l = m +1;
+				else
+					r = m;
+			}
+			
+			return l;
+		}
+	
+	}
