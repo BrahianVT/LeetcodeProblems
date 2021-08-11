49db4cdeb4e1251431fd63fdac7a5c143175aa72
BrahianVT <pumasemj@hotmail.com>
2021-08-10 20:00:24 -0600

    added problem 5

---
+++ src/main/java/leetcode/solutions/August/StoneGame.java
@@ -0,0 +1,24 @@
+package leetcode.solutions;
+import java.util.*;
+/*
+	Problem : 5
+	Number leecode problem: 877
+	https://leetcode.com/problems/stone-game/
+    Time Complexity: O(n2)
+    Space Complexity: O(n2)
+*/
+
+
+public class StoneGame{
+	public boolean stoneGame(int[] piles) {
+        int n = piles.length;
+		int[][] dp = new int[n][n];
+		for(int i = 0; i < n; i++)dp[i][i] = i;
+		
+		for(int d = 1; d < n; d++){
+			for(int i = 0; i < n-d; i++){
+				dp[i][i + d] = Math.max(p[i] - dp[i +1][i + d], p[i + d] - dp[i][i - d - i]);
+			}
+		}
+    }
+}
