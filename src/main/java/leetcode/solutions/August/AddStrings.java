c49230a43ec33aba7687ddcf470152286d1c8fc6
BrahianVT <pumasemj@hotmail.com>
2021-08-16 20:19:09 -0600

    added problem 9

---
+++ src/main/java/leetcode/solutions/August/AddStrings.java
@@ -0,0 +1,26 @@
+package leetcode.solutions;
+import java.util.*;
+/*
+	Problem : 9
+	Number leecode problem: 415
+	https://leetcode.com/problems/add-strings/
+    Time Complexity: O(n)
+    Space Complexity: O(n)
+*/
+
+public class  AddStrings{
+	public String addStrings(String num1, String num2) {
+		StringBuilder res =  new StringBuilder();
+		 int i = num1.length()-1, num2.length()-1;
+		 int sum = 0;
+		 while( i >= 0 || j >= 0){
+			 if(i >= 0)sum+=num1.charAt(i--) - '0';
+			 if(j >= 0)sum+=num2.charAt(j--) - '0';
+			 res.insert(0, sum%10);
+			 sum/=10;
+		 }
+		 if(sum > 0)res.insert(0, sum);
+		 
+		 return res.toString();
+    }
+}
