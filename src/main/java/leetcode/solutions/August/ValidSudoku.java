0b204e212990ab5e3aa6d23c60d406765265a288
BrahianVT <pumasemj@hotmail.com>
2021-08-30 18:32:18 -0600

    added 20

---
+++ src/main/java/leetcode/solutions/August/ValidSudoku.java
@@ -0,0 +1,35 @@
+package leetcode.solutions;
+import java.util.*;
+/*
+	Problem : 20
+	Number leecode problem: 36
+	https://leetcode.com/problems/valid-sudoku/
+    Time Complexity: O(n)
+    Space Complexity: O(n)
+*/
+
+public class ValidSudoku{
+	public boolean isValidSudoku(char[][] board) {
+		boolean[][] rows = new boolean[10][10], cols = new boolean[10][10], sectors = new boolean[10][10]; 
+		
+		int auxSector = 1;
+		for(int i = 0; i < 3; i++){
+			for(int j = 0; j < 3; j++){
+				int i2 = i + 2*i, j2 = j + 2*j
+				for(int r = i2; r < i2 + 3; r++){
+					for(int c = j2; c < j2+ 3; c++){
+						if(board[r][c] != '.'){
+							int ele = board[r][c] - '0';
+							if(rows[r + 1][ele] || cols[c +1][ele] || sectors[auxSector][ele])return false;
+							rows[r + 1][ele] = true;
+							cols[c +1][ele] = true;
+							sectors[auxSector][ele] = true;
+						}
+					}
+				}
+			}
+			auxSector++;
+		}
+		return true;
+    }
+}
